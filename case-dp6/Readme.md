4b2f6b47eab91da5cec074c847257d7d8f600a4c
Breno Navarro <69862291+BrenoNavarro@users.noreply.github.com>
2020-12-30 15:25:07 -0300

    Add files via upload

---
+++ case-dp6/Readme.md
@@ -0,0 +1,42 @@
+# Case Técnico da DP6
+
+Implementação de uma coleta de dados para o Google Analytics.
+
+Este ambiente foi implementado em HTML5, CSS3 e Javascript, a execução é realizada utilizando o Node.Js. 
+
+### Instalação do NodeJs
+
+O <i>[download](https://www.google.com)</i> do Node.Js pode ser feito no site oficial. Após o <i>download</i>, realize a instalação do pacote <i>Serve</i> usando:
+
+npm install -g serve
+
+Depois de instalado, execute o comando <i>serve</i> em alguma terminal, cmd, powershell ou terminal do editor de código.
+
+![image-20201230144744203](C:\Users\Breno\AppData\Roaming\Typora\typora-user-images\image-20201230144744203.png)
+
+
+
+### Google Analytics Debugger
+
+Além disso, para debuggar foi utilizado a extensão <i>[Google Analytics Debugger](https://chrome.google.com/webstore/detail/google-analytics-debugger/jnkmfdileelhofjcijamephohjechhna)</i>, por onde, a quem interessar possa conferir os envios dos dados. 
+
+![image-20201230150108159](C:\Users\Breno\AppData\Roaming\Typora\typora-user-images\image-20201230150108159.png)
+
+
+
+## Case
+
+Os dados foram enviados utilizando seguindo a biblioteca do <i>[analytics.js](https://developers.google.com/analytics/devguides/collection/analyticsjs?hl=pt-br)</i> com auxílio dos disparos de envento do <i>JavaScript</i>.
+
+#### Todas as páginas
+
+Em todas as páginas há o envio de "visualização de página", caso o usuário clique, no menu, em "entre em contato" e/ou "Download PDF".
+
+#### Na página análise
+
+Na página de análise, o envio ocorre caso o usuário clique em um ou mais <i>cards</i> contidos na página, além disso o rótulo a ser enviado contém o texto do <i>card</i> específico.
+
+#### Na página sobre
+
+Na página sobre, o envio ocorre em cada campo do formulário preenchido pelo usuário, a ação contida no envio depende do id do campo em específico. Outro envio ocorrerá quando o usuário utilizar o botão enviar.
+
---
+++ case-dp6/js/tagueamento.js
@@ -0,0 +1,80 @@
+// Preencha este arquivo com qualquer código que você necessite para realizar a
+// coleta, desde a biblioteca analytics.js, gtag.js ou o snippet do Google Tag 
+// Manager. No último caso, não é necessário implementar a tag <noscript>.
+// O ambiente dispõe da jQuery 3.5.1, então caso deseje, poderá utilizá-la
+// para fazer a sua coleta.
+// Caso tenha alguma dúvida sobre o case, não hesite em entrar em contato.
+
+// Eventos em todas as páginas
+ga('create', 'UA-12345-6', 'auto');
+ga('send', 'pageview')
+
+function addListener(element, type, callback) {
+    if (element.addEventListener) element.addEventListener(type, callback);
+    else if (element.attachEvent) element.attachEvent('on' + type, callback);
+}
+
+var entreContato = document.getElementById('contato');
+var download = document.getElementById('download');
+
+addListener(entreContato, 'click', function() {
+    ga('send', 'event', {
+        'eventCategory': 'menu',
+        'eventAction': 'entre_em_contato',
+        'eventLabel': 'link_externo',
+    });
+});
+
+addListener(download, 'click', function() {
+    ga('send', 'event', {
+        'eventCategory': 'menu',
+        'eventAction': 'download_pdf',
+        'eventLabel': 'download_pdf',
+    });
+});
+
+
+// Eventos da página Análise
+if (location.pathname === '/analise') {
+
+    var cardId = '';
+
+    function clickou(clickedId) {
+        cardId = document.getElementById(clickedId);
+        ga('send', 'event', {
+            'eventCategory': 'analise',
+            'eventAction': 'ver_mais',
+            'eventLabel': cardId.getElementsByTagName('p')[0].textContent,
+        });
+    }
+}
+
+// Eventos da página sobre
+if (location.pathname === '/sobre') {
+
+    var cardId = '';
+    var enviarForm = document.getElementById('enviar');
+
+    function clickou(clickedId) {
+
+        cardId = document.getElementById(clickedId);
+        cardId.onchange = function() {
+            ga('send', 'event', {
+                'eventCategory': 'contato',
+                'eventAction': cardId.id,
+                'eventLabel': 'preencheu',
+            });
+        };
+    }
+
+    addListener(enviarForm, 'click', function() {
+        setTimeout(function() {
+            ga('send', 'event', {
+                'eventCategory': 'contato',
+                'eventAction': 'enviado',
+                'eventLabel': 'enviado',
+            });
+        }, 2000);
+    })
+
+};
